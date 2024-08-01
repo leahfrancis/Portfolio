5e5e156668f140f245f1d6563cb267bb60a68ae8
leah <leahcalicut@gmail.com>
2024-08-01 23:44:25 +0530

    modified

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -15,7 +15,7 @@ const WorkCard = () => {
         <h1 className='project-heading'>Projects</h1>
         <div className='project-container'>
             <div className='project-card'>
-                <img src={pro1} alt='photo'/>
+                <img src={pro1} alt='let it out photo'/>
                 <h2 className='project-title'>Let It Out-Mental Health Ease App</h2>
                 <div className='proj-details'>
                   <p>Android application. 
@@ -29,8 +29,8 @@ const WorkCard = () => {
                 </div>
             </div>
             <div className='project-card'>
-                <img src={pro3} alt='photo'/>
-                <img src={pro4} alt='photo'/>
+                <img src={pro3} alt='mosquito photo'/>
+                <img src={pro4} alt='mosquito photo2'/>
                 <h2 className='project-title'>Smart Mosquito Species and Density Detection</h2>
                 <div className='proj-details'>
                   <p>IoT based system developed to lure and trap mosquitos and identify their species and density.
@@ -42,7 +42,7 @@ const WorkCard = () => {
                 </div>
             </div>
             <div className='project-card'>
-                <img src={pro2} alt='photo'/>
+                <img src={pro2} alt='bubble app'/>
                 
                 <h2 className='project-title'>Bubble and Arrow App using JavaScript</h2>
                 <div className='proj-details'>
@@ -55,8 +55,8 @@ const WorkCard = () => {
                 </div>
             </div>
             <div className='project-card'>
-                <img src={pro5} alt='photo'/>
-                <img src={pro6} alt='photo'/>
+                <img src={pro5} alt='netflix1'/>
+                <img src={pro6} alt='netflix2'/>
                 
                 <h2 className='project-title'>Netflix Clone Website</h2>
                 <div className='proj-details'>
@@ -68,7 +68,7 @@ const WorkCard = () => {
                 </div>
             </div>
             <div className='project-card'>
-                <img src={pro7} alt='photo'/>
+                <img src={pro7} alt='state table'/>
                
                 
                 <h2 className='project-title'>State Table for Product Sale.</h2>
@@ -81,7 +81,7 @@ const WorkCard = () => {
                 </div>
             </div>
             <div className='project-card'>
-                <img src={pro8} alt='photo'/>
+                <img src={pro8} alt='E commerce'/>
                
                 
                 <h2 className='project-title'>E-Commerce Website</h2>
