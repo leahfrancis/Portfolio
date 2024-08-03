fe46dc14bdd9c442973fee5c7826294d4efaab02
leah <leahcalicut@gmail.com>
2024-08-03 14:09:49 +0530

    update form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,47 +1,26 @@
+import React from 'react';
 import './Form.css';
-import React, { useState } from 'react';
 
 const Form = () => {
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const data = new FormData(form);
-    try {
-      await fetch('/', {
-        method: 'POST',
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(data).toString(),
-      });
-      setSubmitted(true);
-    } catch (error) {
-      console.error('Form submission error:', error);
-    }
-  };
-
   return (
     <div className='form'>
-      {submitted ? (
-        <div>
-          <h1>Thank you for your submission!</h1>
-          <p>We have received your message and will get back to you shortly.</p>
-        </div>
-      ) : (
-        <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" onSubmit={handleSubmit}>
-          <input type='hidden' name='form-name' value='contact' />
-          <input type='hidden' name='bot-field' />
-          <label>Your Name:</label>
-          <input type='text' name='fname' required />
-          <label htmlFor='email'>Email:</label>
-          <input type='email' name='email' id='email' required />
-          <label>Subject:</label>
-          <input type='text' name='subject' required />
-          <label>Message:</label>
-          <textarea rows="6" name='message' placeholder='Type your message here' required></textarea>
-          <button className='btn' type='submit'>Submit</button>
-        </form>
-      )}
+      <form action="/success.html" method="POST" data-netlify="true">
+        <input type='hidden' name='form-name' value='contact' />
+
+        <label>Your Name:</label>
+        <input type='text' name='fname' required />
+
+        <label htmlFor='email'>Email:</label>
+        <input type='email' name='email' id='email' required />
+
+        <label>Subject:</label>
+        <input type='text' name='subject' required />
+
+        <label>Message:</label>
+        <textarea rows="6" name='message' placeholder='Type your message here' required></textarea>
+
+        <button className='btn' type='submit'>Submit</button>
+      </form>
     </div>
   );
 };
