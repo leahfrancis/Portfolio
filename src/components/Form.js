65e0a1f06cda1228df54826cf282199920545093
leah <leahcalicut@gmail.com>
2024-08-03 12:20:51 +0530

    form2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,23 @@
-import './Form.css'
-
-import React from 'react'
+import './Form.css';
+import React from 'react';
 
 const Form = () => {
   return (
     <div className='form'>
-        <form name="contact v1" method="post" data-netlify="true" onSubmit="submit">
-          <input type='hidden' name='form-name' value='contact v1'/>
-            <label>Your Name:</label>
-            <input type='text' name='fname'></input>
-            <label htmlFor='email'>Email:</label>
-            <input type='email' id='email'></input>
-            <label>Subject:</label>
-            <input type='text' name='subject'></input>
-            <label>Message:</label>
-            <textarea rows="6" placeholder='Type your message here' name='message'></textarea>
-            <button className='btn' type='submit'>Submit</button>
-
-        </form>
+      <form name="contact" method="POST" data-netlify="true">
+        <input type='hidden' name='form-name' value='contact' />
+        <label>Your Name:</label>
+        <input type='text' name='fname' required />
+        <label htmlFor='email'>Email:</label>
+        <input type='email' name='email' id='email' required />
+        <label>Subject:</label>
+        <input type='text' name='subject' required />
+        <label>Message:</label>
+        <textarea rows="6" name='message' placeholder='Type your message here' required></textarea>
+        <button className='btn' type='submit'>Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
